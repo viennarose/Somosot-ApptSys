cc6f70b33b2debb975d10587129e484d5d40f1b2
Vienna <90304389+viennarose@users.noreply.github.com>
2022-09-08 14:27:17 +0800

    unfinished
    unfinished login

---
+++ resources/views/livewire/auth/register.blade.php
@@ -0,0 +1,46 @@
+<div>
+    <div class="container col-md-4 offset-md-4 mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h1>Register</h1>
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="mb-3 flex-nowrap input-group">
+                        <span class="input-group-text" id="bi bi-person-fill">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                          </svg>
+                        </span>
+                      <input type="text" class="form-control" id="fullName" placeholder="Full Name">
+                    </div>
+                    <div class="mb-3 flex-nowrap input-group">
+                        <span class="input-group-text" id="bi bi-envelope-fill">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                            </svg>
+                        </span>
+                      <input type="text" class="form-control" id="email" placeholder="Email">
+                    </div>
+                    <div class="mb-3 flex-nowrap input-group">
+                        <span class="input-group-text" id="bi bi-key-fill">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                            </svg>
+                        </span>
+                        <input type="password" class="form-control" id="password" placeholder="Password">
+                    </div>
+                    <div class="mb-3 flex-nowrap input-group">
+                        <span class="input-group-text" id="bi bi-key-fill">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                            </svg>
+                        </span>
+                        <input type="password" class="form-control" id="password" placeholder="Confirm Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Register</button>
+                  </form>
+            </div>
+        </div>
+    </div>
+</div>
