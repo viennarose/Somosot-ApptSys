e9c408b25dbd748759eb88294116404fa46bb5ce
Vienna <90304389+viennarose@users.noreply.github.com>
2022-09-13 01:47:17 +0800

    auth-done
    views inside log in

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -6,6 +6,19 @@ use Livewire\Component;
 
 class Login extends Component
 {
+    public $email, $password;
+
+    public function submit(){
+        $this->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        Auth::attempt(['email' =>$this->email, 'password' => $this->password]);
+
+        return redirect(route('home'));
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Auth\Login;
+use App\Http\Livewire\Auth\Register;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('adminbase');
 });
-Route::get('/login', [AuthController::class, 'login']); //No Controller yet
+*/
+
+//Route::get('/home', 'home')->name('home');//No Controller yet
+Route::get('/login', Login::class)->name('login');
+Route::get('/register', Register::class)->name('register');
